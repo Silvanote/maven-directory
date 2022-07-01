0b4be2f494d72a11d0b4e0dc09e718b7b54ba75e
Juan Silva <juan.silvawd@gmail.com>
2022-07-01 13:41:40 -0500

    first push

---
+++ src/main/java/MavenEx.java
@@ -0,0 +1,28 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+class MavenEx{
+
+public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter username");
+        String exampleA = "";
+                String exampleB = "";
+//        if(StringUtils.isNumeric(userInput)){
+//                System.out.println("You entered a number!");
+//        } else {
+//                revFlip = StringUtils.reverse(StringUtils.swapCase(userInput));
+//        }
+        String userName = myObj.nextLine();  // Read user input
+        System.out.println("Username is: " + userName);
+
+
+        // Output user input
+//        Enter Something: Life, The Universe, and Everything
+//        You Entered: "Life, The Universe, and Everything"
+//        "Life, The Universe, and Everything" is not a number
+//        Flipped Case: lIFE, tHE uNIVERSE, AND eVERYTHING
+//        Reversed: gnihtyrevE dna ,esrevinU eht ,efiL
+        }
+        }
---
+++ src/main/java/test.java
@@ -0,0 +1,10 @@
+import org.apache.commons.collections4.MultiSet;
+import org.apache.commons.collections4.multiset.HashMultiSet;
+
+public class test {
+    MultiSet<String>multiSet = new HashMultiSet<>();
+
+    public static void main(String[] args) {
+
+    }
+}
